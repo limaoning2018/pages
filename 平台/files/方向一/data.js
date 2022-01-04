e870caf3d586e34075698ff72c707e66fa6c1f5c
limaoning2018 <38202751+limaoning2018@users.noreply.github.com>
2022-01-04 15:22:45 +0800

    Add files via upload

---
+++ 平台/files/方向一/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(p,q),r,[s],t,_(u,v,w,x,g,y,z,_(),A,[],B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,N,N,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(H,I,J,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,bk,bl,J,_(bm,bn,bo,bn,bp,bn,bq,br)),i,_(j,bl,l,bl)),bs,_(),bt,_(),bu,_(bv,[])),bw,_(),bx,_());}; 
+var b="url",c="方向一.html",d="generationDate",e=new Date(1641279868055.51),f="defaultAdaptiveView",g="name",h="1920*1080",i="size",j="width",k=1920,l="height",m=1080,n="adaptiveViews",o="sketchKeys",p="",q="s0",r="variables",s="OnLoadVariable",t="page",u="packageId",v="aba9bc2c236e45bebc11eef8490557e8",w="type",x="Axure:Page",y="方向一",z="notes",A="annotations",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="center",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageAlignment",N="near",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="spread",bl=0,bm="r",bn=0,bo="g",bp="b",bq="a",br=0.349019607843137,bs="adaptiveStyles",bt="interactionMap",bu="diagram",bv="objects",bw="masters",bx="objectPaths";
+return _creator();
+})());
